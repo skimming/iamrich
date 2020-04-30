09279ec38c7fc2f942657bbd4c3d8f2c34a77600
Sam <samuel_w_kim@hotmail.com>
2020-04-29 20:18:46 -0700

    first

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Samuel Kim on 4/29/20.
+//  Copyright © 2020 Samuel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
